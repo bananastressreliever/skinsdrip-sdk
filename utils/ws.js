2b32ad1c529be8e3b0a7143ff2d17c47c1e32705
JonasTheCuriousDev <40566161+BroderTheCuriousDev@users.noreply.github.com>
2024-05-20 11:28:44 +0200

    chore: Add retry and ping intervals to WebSocket connection in WS class

--- utils/ws.js
+++ utils/ws.js
@@ -18,85 +18,108 @@ export default class WS extends EventEmitter {
 
         this.init()
 
+        this.retryInterval = 7500;
+        this.pingInterval = 7000;
+
+        // Timeout and interval IDs
+
+        this.retryTimeoutId = null;
+        this.pingIntervalId = null;
+
     }
 
     init = () => {
         if (!this.cookie) throw new Error("Cookie is required");
 
-        this.subscribe()
+        this.connect()
 
     }
 
-    /**
-     * Establishes a WebSocket connection and subscribes to events.
-     */
-    subscribe = () => {
-        const retryInterval = 7500;
-        const pingInterval = 7000;
+    setupWebSocketEvents = () => {
 
-        const connect = () => {
-            console.log("Trying to connect to the Skinsdrip wss server...");
+        this.ws.on('open', () => {
+            console.log("Connected to the Skinsdrip wss server");
+            this.isConnected = true;
 
-            if (!this.isConnected || !this.ws) {
+            // Clear any existing ping interval
+            if (this.pingIntervalId) {
+                clearInterval(this.pingIntervalId);
+            }
+
+            // Send a ping message every 7 seconds
+            this.pingIntervalId = setInterval(() => {
+                if (this.ws && this.isConnected) {
+                    this.ws.send('ping');
+                }
+            }, this.pingInterval);
+        });
+
+        this.ws.on('message', (buffer) => {
+            const dataStr = buffer?.toString(); // Convert buffer to string
+            let data;
+            try {
+                data = JSON.parse(dataStr || '{}');
+            } catch (error) {
+                console.log("ERROR PARSING MESSAGE", error);
+                return;
+            }
+
+            const event = data?.event;
+
+            if (event?.includes("merchant")) data.event = event?.split('merchant:')?.[1];
+
+            if (event?.includes("trade:update")) {
                 try {
-                    this.ws = new WebSocket(this.baseUrl, {
-                        headers: {
-                            'Cookie': `auth=${this.cookie}`
-                        }
-                    });
+                    const orderId = data.data.orderId;
+                    injector.removePendingCallback(orderId);
                 } catch (error) {
-                    console.log("ERROR CONNECTING TO WS", error);
-                    setTimeout(connect, retryInterval);
-                    return;
+                    console.log("ERROR remove pending callback", error);
                 }
             }
 
-            this.ws.on('open', () => {
-                console.log("Connected to the Skinsdrip wss server");
-                this.isConnected = true;
+            this.emit(data.event, data);
+        });
 
-                // Send a ping message every 7 seconds
-                setInterval(() => {
-                    if (this.ws && this.isConnected) {
-                        this.ws.send('ping');
+        this.ws.on('close', () => {
+            console.log('disconnected from the Skinsdrip wss server');
+            this.isConnected = false;
+
+            // Clear any existing retry timeout
+            if (this.retryTimeoutId) {
+                clearTimeout(this.retryTimeoutId);
+            }
+
+            // Clear any existing ping interval
+            if (this.pingIntervalId) {
+                clearInterval(this.pingIntervalId);
+            }
+
+            // Try to reconnect after a delay
+            this.retryTimeoutId = setTimeout(this.connect, this.retryInterval);
+        });
+    };
+
+    connect = () => {
+        if (!this.isConnected || !this.ws) {
+            try {
+                this.ws = new WebSocket(this.baseUrl, {
+                    headers: {
+                        'Cookie': `auth=${this.cookie}`
                     }
-                }, pingInterval);
-            });
+                });
+                this.setupWebSocketEvents();
+            } catch (error) {
+                console.log("ERROR CONNECTING TO WS", error);
 
-            this.ws.on('message', (buffer) => {
-                const dataStr = buffer?.toString(); // Convert buffer to string
-                let data;
-                try {
-                    data = JSON.parse(dataStr || '{}');
-                } catch (error) {
-                    console.log("ERROR PARSING MESSAGE", error);
-                    return;
+                // Clear any existing retry timeout
+                if (this.retryTimeoutId) {
+                    clearTimeout(this.retryTimeoutId);
                 }
 
-                const event = data?.event;
-
-                if (event?.includes("merchant")) data.event = event?.split('merchant:')?.[1];
-
-                if (event?.includes("trade:update")) {
-                    try {
-                        const orderId = data.data.orderId;
-                        injector.removePendingCallback(orderId);
-                    } catch (error) {
-                        console.log("ERROR remove pending callback", error);
-                    }
-                }
-
-                this.emit(data.event, data);
-            });
-
-            this.ws.on('close', () => {
-                console.log('disconnected from the Skinsdrip wss server');
-                this.isConnected = false;
-                setTimeout(connect, retryInterval);
-            });
-        };
-
-        connect();
-    }
+                // Try to reconnect after a delay
+                this.retryTimeoutId = setTimeout(this.connect, this.retryInterval);
+            }
+        }
+    };
 
 }
